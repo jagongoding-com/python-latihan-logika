4eec11d0c590dec3e055488ae3ac711248b3ac07
Nurul Huda <ibnujakaria@outlook.com>
2021-04-19 22:51:04 +0700

    kalkulator sederhana

---
+++ 15-kalkulator-sederhana/01-step-1-inputan.py
@@ -0,0 +1,11 @@
+print('=' * 25)
+print('Operasi Matematika')
+print('  1. Jumlah \t [+]')
+print('  2. Kurang \t [-]')
+print('  3. Kali \t [*]')
+print('  4. Bagi \t [/]')
+print('=' * 25)
+
+operasi = input('Pilih operasi (1/2/3/4): ')
+bilangan_1 = eval(input('Masukkan bilangan pertama: '))
+bilangan_2 = eval(input('Masukkan bilangan kedua: '))
---
+++ 15-kalkulator-sederhana/02-step-2-percabangan-dasar.py
@@ -0,0 +1,22 @@
+print('=' * 25)
+print('Operasi Matematika')
+print('  1. Jumlah \t [+]')
+print('  2. Kurang \t [-]')
+print('  3. Kali \t [*]')
+print('  4. Bagi \t [/]')
+print('=' * 25)
+
+operasi = input('Pilih operasi (1/2/3/4): ')
+bilangan_1 = eval(input('Masukkan bilangan pertama: '))
+bilangan_2 = eval(input('Masukkan bilangan kedua: '))
+
+if operasi == '1':
+  print('User memilih penjumlahan')
+elif operasi == '2':
+  print('User memilih pengurangan')
+elif operasi == '3':
+  print('User memilih perkalian')
+elif operasi == '4':
+  print('User memilih pembagian')
+else:
+  print('Tidak valid')
