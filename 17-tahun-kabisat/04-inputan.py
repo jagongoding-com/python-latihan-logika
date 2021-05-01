05408df483aa8e5dd983ce2b911d8d98a9cc4f5e
Nurul Huda <ibnujakaria@outlook.com>
2021-05-01 12:54:07 +0700

    ups, salah edit file

--- 17-tahun-kabisat/04-inputan.py
+++ 17-tahun-kabisat/04-inputan.py
@@ -3,16 +3,11 @@ def apakah_kabisat (tahun):
   habis_dibagi_100 = tahun % 100 == 0
   habis_dibagi_4 = tahun % 4 == 0
 
-  return habis_dibagi_400 or (habis_dibagi_4 and not habis_dibagi_100)
+  if habis_dibagi_400 or (habis_dibagi_4 and not habis_dibagi_100):
+    print(f'{tahun} tahun kabisat')
+  else:
+    print(f'{tahun} bukan tahun kabisat')
 
-tahun_awal = int(input('Masukkan tahun awal: '))
-tahun_akhir = int(input('Masukkan tahun akhir: '))
+tahun = int(input('Masukkan tahun: '))
 
-tahun_kabisat = []
-
-for tahun in range(tahun_awal, tahun_akhir + 1):
-  if apakah_kabisat(tahun):
-    tahun_kabisat.append(tahun)
-
-print('Tahun kabisat:')
-print(tahun_kabisat)
+apakah_kabisat(tahun)
